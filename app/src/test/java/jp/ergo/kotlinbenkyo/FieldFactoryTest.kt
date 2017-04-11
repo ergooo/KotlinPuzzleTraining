a99732733b620a701c69d48dcf9b4dd047777d6d
masato <goodmoon.badmoon@gmail.com>
2017-04-12 02:33:25 +0900

    テストとコメントを書いた

--- app/src/test/java/jp/ergo/kotlinbenkyo/FieldFactoryTest.kt
+++ app/src/test/java/jp/ergo/kotlinbenkyo/FieldFactoryTest.kt
@@ -6,18 +6,79 @@ import org.junit.Test
 
 class FieldFactoryTest {
     @Test
-    fun create5x5Field() {
+    fun create5x5FieldTest() {
         val testData = (0..24).map { 1 }
-        println("testData.size: " + testData.size)
+        assertThat(testData.size, `is`(25))
         val actual = Field.FieldFactory.create5x5Field(testData)
-        println(actual)
-        actual?.let { it.masus.forEach { println(it) } }
+        val expect = Field(mapOf(
+                Address(0, 0) to Direction.DOWN,
+                Address(0, 1) to Direction.DOWN,
+                Address(0, 2) to Direction.DOWN,
+                Address(0, 3) to Direction.DOWN,
+                Address(0, 4) to Direction.DOWN,
+                Address(1, 0) to Direction.DOWN,
+                Address(1, 1) to Direction.DOWN,
+                Address(1, 2) to Direction.DOWN,
+                Address(1, 3) to Direction.DOWN,
+                Address(1, 4) to Direction.DOWN,
+                Address(2, 0) to Direction.DOWN,
+                Address(2, 1) to Direction.DOWN,
+                Address(2, 2) to Direction.DOWN,
+                Address(2, 3) to Direction.DOWN,
+                Address(2, 4) to Direction.DOWN,
+                Address(3, 0) to Direction.DOWN,
+                Address(3, 1) to Direction.DOWN,
+                Address(3, 2) to Direction.DOWN,
+                Address(3, 3) to Direction.DOWN,
+                Address(3, 4) to Direction.DOWN,
+                Address(4, 0) to Direction.DOWN,
+                Address(4, 1) to Direction.DOWN,
+                Address(4, 2) to Direction.DOWN,
+                Address(4, 3) to Direction.DOWN,
+                Address(4, 4) to Direction.DOWN
+        ))
+        assertThat(actual, `is`(expect))
 
-        actual?.let {
-            val list = it.trace(Address(0, 0))
-            println("lis: " + list)
-        }
+    }
 
+    @Test
+    fun removeFieldTest() {
+        val testData = mapOf(
+                Address(0, 0) to Direction.DOWN,
+                Address(0, 1) to Direction.DOWN,
+                Address(0, 2) to Direction.DOWN,
+                Address(0, 3) to Direction.DOWN,
+                Address(0, 4) to null
+        )
+        val sut = Field(testData)
+        val actual = sut.removedField(listOf(Address(0, 0), Address(0, 1)))
+        val expect = Field(mapOf(
+                Address(0, 0) to null,
+                Address(0, 1) to null,
+                Address(0, 2) to Direction.DOWN,
+                Address(0, 3) to Direction.DOWN,
+                Address(0, 4) to null
+        ))
+        assertThat(actual, `is`(expect))
+    }
+
+    @Test
+    fun traceTest() {
+        val testData = mapOf(
+                Address(0, 0) to Direction.DOWN,
+                Address(0, 1) to Direction.DOWN,
+                Address(0, 2) to Direction.DOWN,
+                Address(0, 3) to Direction.DOWN,
+                Address(0, 4) to null
+        )
+        val sut = Field(testData)
+        val actual = sut.trace(Address(0, 0))
+        val expect = listOf(
+                Address(0, 0),
+                Address(0, 1),
+                Address(0, 2),
+                Address(0, 3))
+        assertThat(actual, `is`(expect))
     }
 
     @Test

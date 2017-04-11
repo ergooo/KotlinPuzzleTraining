152d3e577ed7f5150f55d98a0e763a545dd58340
masato <goodmoon.badmoon@gmail.com>
2017-04-12 02:33:25 +0900

    テストとコメントを書いた

--- app/src/main/java/jp/ergo/kotlinbenkyo/Puzzle.kt
+++ app/src/main/java/jp/ergo/kotlinbenkyo/Puzzle.kt
@@ -63,6 +63,19 @@ data class Address(val x: Int, val y: Int) {
 
 class Field internal constructor(val masus: Map<Address, Direction?>) {
 
+    companion object FieldFactory {
+        fun create5x5Field(rawDirections: List<Int>): Field? {
+            if (rawDirections.size != 25) return null
+            val directions = rawDirections.map { Direction.of(it) }
+            val addresses = (0..4).map { x -> (0..4).map { y -> Address(x, y) } }.flatten()
+            val masus = addresses.zip(directions, ::Pair).associate { it.first to it.second }
+            return Field(masus)
+        }
+    }
+
+    /**
+     * @return 指定したAddressの示す次のAddressを返す。次のAddressが範囲外であればnull
+     */
     fun nextTo(address: Address): Address? {
         return when (masus[address]) {
             Direction.RIGHT -> address.right()
@@ -73,18 +86,20 @@ class Field internal constructor(val masus: Map<Address, Direction?>) {
         }
     }
 
+    /**
+     * @return 指定したアドレスリストのDirectionをnullにしたFieldを返す。
+     */
     fun removedField(address: List<Address>): Field {
         val removedMasus = masus.map { Pair(it.key, if (address.contains(it.key)) null else it.value) }.associate { it.first to it.second }
         return Field(removedMasus)
     }
 
-    fun moveDownField(): Field {
-        return moveField(masus, canDown, slideDown)
-    }
-
-    tailrec fun moveField(masus: Map<Address, Direction?>,
-                  canMove: (Map<Address, Direction?>, Address) -> Boolean,
-                  slide: (Address, Direction?) -> List<Pair<Address, Direction?>>): Field {
+    /**
+     * @return 自身のMasus内にcanMoveに適合する要素が含まれている場合、再帰的にslideに適応させた要素で置き換えたFieldを返す。
+     */
+    tailrec private fun moveField(masus: Map<Address, Direction?>,
+                          canMove: (Map<Address, Direction?>, Address) -> Boolean,
+                          slide: (Address, Direction?) -> List<Pair<Address, Direction?>>): Field {
         val filtered = masus.filter { canMove(masus, it.key) }
         return when {
             filtered.isEmpty() -> Field(masus)
@@ -98,48 +113,53 @@ class Field internal constructor(val masus: Map<Address, Direction?>) {
         }
     }
 
-    val slideDown: (Address, Direction?) -> List<Pair<Address, Direction?>> = { address: Address, direction: Direction? ->
+    /**
+     * @return Directionがnullである要素を下詰にしたFieldを返す
+     */
+    fun moveDownField(): Field {
+        return moveField(masus, canDown, slideDown)
+    }
+
+
+    private val slideDown: (Address, Direction?) -> List<Pair<Address, Direction?>> = { address: Address, direction: Direction? ->
         listOf(address to null, address.down()!! to direction)
     }
 
-    val canDown: (Map<Address, Direction?>, Address) -> Boolean = { masus: Map<Address, Direction?>, address: Address ->
+    private val canDown: (Map<Address, Direction?>, Address) -> Boolean = { masus: Map<Address, Direction?>, address: Address ->
         masus[address.down()] == null && !address.isBottomEdge()
     }
 
+    /**
+     * @return Directionがnullである要素を左詰めにしたFieldを返す。
+     */
     fun moveLeftField(): Field {
         return moveField(masus, canLeft, slideLeft)
     }
 
-    val slideLeft: (Address, Direction?) ->  List<Pair<Address, Direction?>> = {address: Address, direction: Direction? ->
+    private val slideLeft: (Address, Direction?) -> List<Pair<Address, Direction?>> = { address: Address, direction: Direction? ->
         listOf(address to null, address.left()!! to direction)
     }
 
-    val canLeft: (Map<Address, Direction?>, Address) -> Boolean = { masus: Map<Address, Direction?>, address: Address ->
+    private val canLeft: (Map<Address, Direction?>, Address) -> Boolean = { masus: Map<Address, Direction?>, address: Address ->
         masus[address.left()] == null && !address.isLeftEdge()
     }
 
-    companion object FieldFactory {
-        fun create5x5Field(rawDirections: List<Int>): Field? {
-            if (rawDirections.size != 25) return null
-            val directions = rawDirections.map { Direction.of(it) }
-            val addresses = (0..4).map { x -> (0..4).map { y -> Address(x, y) } }.flatten()
-            val masus = addresses.zip(directions, ::Pair).associate { it.first to it.second }
-            return Field(masus)
-        }
-    }
-
+    /**
+     * @return 指定したAddressからDirectionを辿ったAddressのリストを返す。
+     */
     fun trace(address: Address): List<Address> {
-        return trace(listOf(), address)
+        return trace(listOf(address), address)
     }
 
-    private fun trace(acc: List<Address>, address: Address): List<Address> {
+    tailrec private fun trace(acc: List<Address>, address: Address): List<Address> {
         val next = nextTo(address)
-        println(next)
-        return when (next) {
-            null -> acc
+        return when {
+            next == null -> acc
+            masus[next] == null -> acc
             else -> trace(acc + (next), next)
         }
     }
+
 
     override fun toString(): String {
         return "Field(masus=$masus)"

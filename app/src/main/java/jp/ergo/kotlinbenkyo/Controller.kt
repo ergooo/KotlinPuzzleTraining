de48ee28a31bf073026587b1ecdbf672df912362
masato <goodmoon.badmoon@gmail.com>
2017-04-17 21:07:42 +0900

    試行錯誤

--- app/src/main/java/jp/ergo/kotlinbenkyo/Controller.kt
+++ app/src/main/java/jp/ergo/kotlinbenkyo/Controller.kt
@@ -1,43 +1,118 @@
 package jp.ergo.kotlinbenkyo
 
+import jp.ergo.kotlinbenkyo.Controller.Companion.hoge
 import jp.ergo.kotlinbenkyo.Controller.Companion.toCollapsedMap
 
 
 fun main(args: Array<String>) {
     val input = "0000120011333113332133222"
-    val field = Field.FieldFactory.create5x5Field(Controller.convertInput(input))!!
-    val collapsedMap = Controller.toCollapsedMap(field)
-    
-    val path = mutableListOf<Address>()
-    
-    val hoge = toCollapsedMap(collapsedMap.keys.first())
-    path += collapsedMap[collapsedMap.keys.first()]!!
-    
-    val mage = toCollapsedMap(hoge.keys.first())
-    val piyo = toCollapsedMap(mage.keys.first())
-    val fuga = toCollapsedMap(piyo.keys.first())
-    val hogehoge = toCollapsedMap(fuga.keys.first())
-    val magemage = toCollapsedMap(hogehoge.keys.first())
-    val piyopiyo = toCollapsedMap(magemage.keys.first())
-    val fugafuga = toCollapsedMap(piyopiyo.keys.first())
-    val ho = toCollapsedMap(fugafuga.keys.first())
-    val mo = toCollapsedMap(ho.keys.first())
-    
-    if (mo.filter { it.key.isEmpty() }.isNotEmpty()) {
-        println(ho.keys.first())
-        println("Empty!!")
-        return
-    }
-    
-    println(mo.size)
-    mo.map {
-        println(it.value)
-        println(it.key.toArrowSquare())
+    val field = Field.create5x5Field(Controller.convertInput(input))!!
+    val path = hoge(field, Address(0, 0), listOf())
+    println(path)
+
+
+    fun gomi() {
+        val input = "0000120011333113332133222"
+        val field = Field.create5x5Field(Controller.convertInput(input))!!
+        val collapsedMap = Controller.toCollapsedMap(field)
+
+        val path = mutableListOf<Address>()
+
+        var e = collapsedMap.entries.first()
+        val hoge = toCollapsedMap(e.key)
+        path += e.value
+        e = toCollapsedMap(e.key).entries.first()
+        path += e.value
+        e = toCollapsedMap(e.key).entries.first()
+        path += e.value
+        e = toCollapsedMap(e.key).entries.first()
+        path += e.value
+        e = toCollapsedMap(e.key).entries.first()
+        path += e.value
+        e = toCollapsedMap(e.key).entries.first()
+        path += e.value
+        e = toCollapsedMap(e.key).entries.first()
+        path += e.value
+        e = toCollapsedMap(e.key).entries.first()
+        path += e.value
+        e = toCollapsedMap(e.key).entries.first()
+        path += e.value
+        e = toCollapsedMap(e.key).entries.first()
+        path += e.value
+        var mo = toCollapsedMap(e.key)
+        e = mo.entries.first()
+        path += e.value
+        if (mo.filter { it.key.isEmpty() }.isNotEmpty()) {
+            println("Empty!!")
+            println(path)
+            return
+        }
+
+        println(mo.size)
+        mo.map {
+            println(it.value)
+            println(it.key.toArrowSquare())
+        }
     }
 }
 
+
 class Controller {
     companion object {
+        fun hoge(field: Field, address: Address, path: List<Address>): List<Address> {
+            val nextFields = toCollapsedMap(field)
+            val emptyAddress = nextFields[Field.EMPTY]
+            return when (emptyAddress) {
+                null -> {
+                    val e = nextFields.entries.first()
+                    hoge(e.key, e.value, path + address)
+                }
+                else -> path + address + emptyAddress
+            }
+        }
+
+        fun mage(field: Field): List<Address> {
+            val path: List<Address> = listOf()
+            val fl = toCollapsedMap(field)
+            val emptyAddress = fl[Field.EMPTY]
+            when (emptyAddress) {
+                null -> {
+                    val fl2 = fl.map { toCollapsedMap(it.key) }
+                    fl2.forEach { fl ->
+                        val emptyAddress = fl[Field.EMPTY]
+                        if (emptyAddress != null) {
+                            return path + emptyAddress
+                        } else {
+                            val fl3 = fl.map { toCollapsedMap(it.key) }
+                            fl3.forEach {
+                                fl ->
+                                val emptyAddress = fl[Field.EMPTY]
+                                if (emptyAddress != null) {
+                                    return path + emptyAddress
+                                } else {
+                                }
+                            }
+                        }
+                    }
+                }
+                else -> return path + emptyAddress
+            }
+        }
+
+        fun hogehoge(fl: Map<Field, Address>, path: List<Address>): List<Address> {
+            val flList = fl.map { toCollapsedMap(it.key) }
+            flList.forEach { fl ->
+                val emptyAddress = fl[Field.EMPTY]
+                if (emptyAddress != null) {
+                    return@hogehoge path + emptyAddress
+                } else {
+                    return@hogehoge hogehoge(fl, path)
+                }
+            }
+            
+
+        }
+
         /**
          * 文字列をIntのリストに変換する
          */
@@ -59,7 +134,7 @@ class Controller {
                 else -> ge(field.collapseFrom(availables.first()), availables.drop(0), acc + availables.first())
             }
         }
-        
+
         fun getResult(field: Field): List<Address> {
             return listOf()
         }
